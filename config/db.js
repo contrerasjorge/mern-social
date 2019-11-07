24968866186cf0ada7c137e9aa028e3a86413bdf
Jorge Contreras <jorge.contreras789@gmail.com>
2019-11-07 15:27:02 -0500

    Create user API routes and JWT authentication

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true
     });
 
     console.log('🤖 MongoDB Connected...😼');
