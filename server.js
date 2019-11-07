7dd986800893ffeaf2ce4ec3c3183d3536f1e003
Jorge Contreras <jorge.contreras789@gmail.com>
2019-11-07 14:20:26 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running 🤩'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT} 🥳`));
