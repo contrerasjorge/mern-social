fe154ac98e6db87195e3dd1953f0e1c9403e3c3d
Jorge Contreras <jorge.contreras789@gmail.com>
2019-11-08 13:20:12 -0500

    Create profile API routes

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT} 🥳`));
+app.listen(PORT, () => console.log(`🦎 Server started on port ${PORT} 🥳`));
