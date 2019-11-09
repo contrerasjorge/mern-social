6ec1496fcc7814f7931b25e6af0e670847fe4059
Jorge Contreras <jorge.contreras789@gmail.com>
2019-11-09 14:49:00 -0500

    Add education and experience components

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-form/CreateProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 import EditProfile from './components/profile-form/EditProfile';
 
 import './App.css';
@@ -48,6 +50,16 @@ const App = () => {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </section>
